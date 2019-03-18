6ad7c3ccc29bc30b2bc3747d3095c026ccdee102
Emilio Wuerges <wuerges@gmail.com>
2019-03-18 14:48:29 -0300

    token link for capistranio

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, "git@github.com:ccchape/ccchapev3.git"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-set :linked_files, %w{config/master.key}
+set :linked_files, %w{config/master.key TOKEN}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
